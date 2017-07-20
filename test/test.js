497c28d2c8bc4b191bf05559d0ecd2c737e0ecae
eisverticker <eisverticker@gmail.com>
2017-07-20 13:17:12 +0200

    testing: unit tests are now in folder test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 const chai = require('chai');
-const chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised)
+const expect = chai.expect
 
-const MwCategory = require('./index.js')
+const MwCategory = require('../index.js')
 
 const CategoryLoader = MwCategory.CategoryLoader
 const MwSources = MwCategory.MwSources
@@ -11,7 +11,12 @@ const MwSources = MwCategory.MwSources
 describe('#loadMembers()', function() {
   it('should load the category members from wikipedia without an error', function() {
     let loader = CategoryLoader.createFromTemplate(MwSources.Wikipedia, 'en');
-    return loader.loadMembers('Category:Cirties_in_Luxembourg');
+    return loader.loadMembers('Category:Cities_in_Luxembourg');
+  });
+
+  it('should do the same for createFromUrl', function() {
+    let loader = CategoryLoader.createFromUrl('https://en.wikipedia.org/w/api.php');
+    return loader.loadMembers('Category:Cities_in_Luxembourg');
   });
 
   it('should load the category members from wiktionary without an error', function() {
@@ -23,5 +28,11 @@ describe('#loadMembers()', function() {
     let loader = CategoryLoader.createFromTemplate(MwSources.Wikipedia, 'en');
     return expect(loader.loadMembers('Luxembourg'))
       .to.be.rejectedWith('response-body-invalid');
+  });
+
+  it('should throw an error because remote server does not exist', function() {
+    let loader = CategoryLoader.createFromUrl('https://ef.xzsdad.org/w/apdi.php');
+    return expect(loader.loadMembers('Luxembourg'))
+      .to.be.rejected;
   });
 });
