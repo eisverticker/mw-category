76f406814b7151fe502557350a626c071740e4d2
eisv <eisverticker@posteo.de>
2020-10-18 18:29:27 +0200

    test: verify real output of loadMembers

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const citiesInLuxembourg = require('./resources/en-wikipedia-citites-in-luxembourg.json')
 chai.use(chaiAsPromised)
 const expect = chai.expect
 
@@ -11,17 +12,17 @@ const MwSources = MwCategory.MwSources
 describe('#loadMembers()', function() {
   it('should load the category members from wikipedia without an error', function() {
     let loader = CategoryLoader.createFromTemplate(MwSources.Wikipedia, 'en');
-    return loader.loadMembers('Category:Cities_in_Luxembourg');
+    return expect(loader.loadMembers('Category:Cities_in_Luxembourg')).to.eventually.deep.equal(citiesInLuxembourg);
   });
 
   it('should do the same for createFromUrl', function() {
     let loader = CategoryLoader.createFromUrl('https://en.wikipedia.org/w/api.php');
-    return loader.loadMembers('Category:Cities_in_Luxembourg');
+    return expect(loader.loadMembers('Category:Cities_in_Luxembourg')).to.be.fulfilled;
   });
 
   it('should load the category members from wiktionary without an error', function() {
     let loader = CategoryLoader.createFromTemplate(MwSources.Wiktionary, 'en');
-    return loader.loadMembers('Category:Spanish basic words');
+    return expect(loader.loadMembers('Category:Spanish basic words')).to.be.fulfilled;
   });
 
   it('should throw an error because it is no category', function() {
