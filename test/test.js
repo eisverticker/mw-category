78f887e3c248ee197b1485261bb90c7ef1b79bbd
eisv <eisverticker@posteo.de>
2020-10-20 08:29:43 +0200

    build: upgrade got to v11.7

--- test/test.js
+++ test/test.js
@@ -12,28 +12,36 @@ const MwSources = MwCategory.MwSources
 describe('#loadMembers()', function() {
   it('should load the category members from wikipedia without an error', function() {
     let loader = CategoryLoader.createFromTemplate(MwSources.Wikipedia, 'en');
-    return expect(loader.loadMembers('Category:Cities_in_Luxembourg')).to.eventually.deep.equal(citiesInLuxembourg);
+    return expect(
+      loader.loadMembers('Category:Cities_in_Luxembourg')
+    ).to.eventually.deep.equal(citiesInLuxembourg);
   });
 
   it('should do the same for createFromUrl', function() {
     let loader = CategoryLoader.createFromUrl('https://en.wikipedia.org/w/api.php');
-    return expect(loader.loadMembers('Category:Cities_in_Luxembourg')).to.be.fulfilled;
+    return expect(
+      loader.loadMembers('Category:Cities_in_Luxembourg')
+    ).to.be.fulfilled;
   });
 
   it('should load the category members from wiktionary without an error', function() {
     let loader = CategoryLoader.createFromTemplate(MwSources.Wiktionary, 'en');
-    return expect(loader.loadMembers('Category:Spanish basic words')).to.be.fulfilled;
+    return expect(
+      loader.loadMembers('Category:Spanish basic words')
+    ).to.be.fulfilled;
   });
 
   it('should throw an error because it is no category', function() {
     let loader = CategoryLoader.createFromTemplate(MwSources.Wikipedia, 'en');
-    return expect(loader.loadMembers('Luxembourg'))
-      .to.be.rejectedWith('response-body-invalid');
+    return expect(
+      loader.loadMembers('Luxembourg')
+    ).to.be.rejectedWith('response-body-invalid');
   });
 
   it('should throw an error because remote server does not exist', function() {
     let loader = CategoryLoader.createFromUrl('https://ef.xzsdad.org/w/apdi.php');
-    return expect(loader.loadMembers('Luxembourg'))
-      .to.be.rejected;
+    return expect(
+      loader.loadMembers('Luxembourg')
+    ).to.be.rejected;
   });
 });
