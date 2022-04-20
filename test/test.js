d20832730a4aeed53a4ad3f0f6504902f6db9997
eisv <eisverticker@posteo.de>
2022-04-20 21:44:08 +0200

    test: convert test functions to arrow functions

--- test/test.js
+++ test/test.js
@@ -10,21 +10,21 @@ const CategoryLoader = MwCategory.CategoryLoader
 const MwSources = MwCategory.MwSources
 
 describe('#loadMembers()', function() {
-  it('should load category members from wikipedia without an error', function() {
+  it('should load category members from wikipedia without an error', () => {
     let loader = CategoryLoader.createFromTemplate(MwSources.Wikipedia, 'en');
     return expect(
       loader.loadMembers('Category:Cities_in_Luxembourg')
     ).to.eventually.deep.equal(citiesInLuxembourg);
   });
 
-  it('should do the same for createFromUrl', function() {
+  it('should do the same for createFromUrl', () => {
     let loader = CategoryLoader.createFromUrl('https://en.wikipedia.org/w/api.php');
     return expect(
       loader.loadMembers('Category:Cities_in_Luxembourg')
     ).to.be.fulfilled;
   });
 
-  it('should load the category members from wiktionary without an error', function() {
+  it('should load the category members from wiktionary without an error', () => {
     let loader = CategoryLoader.createFromTemplate(MwSources.Wiktionary, 'en');
     return expect(
       loader.loadMembers('Category:Spanish basic words')
@@ -38,7 +38,7 @@ describe('#loadMembers()', function() {
     ).to.be.rejectedWith('response-body-invalid');
   });
 
-  it('should throw an error because remote server does not exist', function() {
+  it('should throw an error because remote server does not exist', () => {
     let loader = CategoryLoader.createFromUrl('https://ef.xzsdad.org/w/apdi.php');
     return expect(
       loader.loadMembers('Luxembourg')
