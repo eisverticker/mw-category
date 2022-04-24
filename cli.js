f4b744d9d633a8baa7f67fd4ac2a56949e868450
eisv <eisverticker@posteo.de>
2022-04-24 20:49:04 +0200

    refactor: improve cli.js
    * update version
    * use async/-await style function

--- cli.js
+++ cli.js
@@ -7,33 +7,31 @@ const mwc = require('./index.js')
 
 const CategoryLoader = mwc.CategoryLoader
 
-// use commander to describe and parse the command (arguments and options)
 program
-  .version('1.2.2')
+  .version('2.0.0')
   .arguments('<url> <title>')
   .option( '-c, --csv', 'Make output csv compatible')
-  .action( function(url, title, env) {
-    // process category
+  .action( async (url, title, env) => {
     const loader = CategoryLoader.createFromUrl(url)
-    // we are trying to load the category members here and just
-    // print the title of it
-    // if an error occurs we print a generic error
-    loader.loadMembers(title)
-      .then(
-        (members) => {
-          members.forEach(
-            (item) => {
-              // csv mode --> add quotation marks
-              if (env.csv) {
-                item.title = '"' + item.title + '"'
-              }
-              console.log(item.title)
-            }
-          )
+
+    let members
+    try {
+      members = await loader.loadMembers(title)
+    } catch (error) {
+      console.error('error', 'program was unable to load category members')
+      return
+    }
+
+    members.forEach(
+      (item) => {
+        // add quotation marks in csv mode
+        if (env.csv) {
+          console.log(`"${item.title}"`)
+          return
         }
-      )
-      .catch(
-        () => console.error('error', 'program was unable to load category members')
-      )
+
+        console.log(item.title)
+      }
+    )
   })
   .parse(process.argv)
